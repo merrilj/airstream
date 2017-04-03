ac0e165e0c7bb1bb5cbb5e8230e54f8fae90283a
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-04-03 09:57:42 -0600

    added semantic-ui

--- airstream/src/App.js
+++ airstream/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Dropdown, Menu, Icon } from 'semantic-ui-react'
 
 class App extends Component {
+  state = { activeItem: 'home' }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render() {
+    const { activeItem } = this.state
     return (
       <div className="App">
+
+        <Menu size='huge'>
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+
+        <Menu.Menu position='right'>
+          <Dropdown item text='Language'>
+            <Dropdown.Menu>
+              <Dropdown.Item>English</Dropdown.Item>
+              <Dropdown.Item>Russian</Dropdown.Item>
+              <Dropdown.Item>Spanish</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Menu.Item>
+            <Button primary animated>
+              <Button.Content visible>Sign Up</Button.Content>
+              <Button.Content hidden>Sign In</Button.Content>
+            </Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>airstream</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <Button primary animated>
+            <Button.Content visible>Next</Button.Content>
+            <Button.Content hidden>
+              <Icon name="right arrow" />
+            </Button.Content>
+          </Button>
+        </div>
       </div>
     );
   }
